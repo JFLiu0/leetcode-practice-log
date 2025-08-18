d3a4600958c34780069c117e691dd0a4dc7ad94d
JFLiu0 <liujf.jeff@gmail.com>
2025-08-19 00:34:36 +0900

    Build Repository Structure- 20250819
    Build Repository Structure- 20250819

---
+++ solutions/01_sliding_window_two_pointers/fixed_length/01456_try1_AC_20250814.py
@@ -0,0 +1,23 @@
+﻿# Problem ID: 01456
+# Title: Maximum Number of Vowels in a Substring of Given Length
+# Category: Sliding Window (Fixed Length)
+# Attempt: 1
+# Initial Result: AC (Accepted)
+# Date: 2025-08-14
+
+class Solution:
+    def maxVowels(self, s: str, k: int) -> int:
+        ans = 0
+        vow = 0
+        for i,ele in enumerate(s):
+            if s[i] in "aeiou":
+                vow += 1
+            if i < k-1:
+                continue
+            
+            ans = max(ans,vow)
+
+            if s[i-k+1] in "aeiou":
+                vow -= 1
+        
+        return ans
